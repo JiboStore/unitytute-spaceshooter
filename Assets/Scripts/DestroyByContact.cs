88110a91694848291181a33cbe6d6055281732e5
Hakim Hauston <jibostore@gmail.com>
2014-10-06 16:29:59 +0800

    scores: http://unity3d.com/learn/tutorials/projects/space-shooter/counting-points

--- Assets/Scripts/DestroyByContact.cs
+++ Assets/Scripts/DestroyByContact.cs
@@ -6,6 +6,20 @@ public class DestroyByContact : MonoBehaviour {
 	public GameObject explosion;
 	public GameObject playerExplosion;
 
+	public int scoreValue;
+	private GameController gameController;
+
+	void Start()
+	{
+		GameObject gameControllerObject = GameObject.FindWithTag ("GameController");
+		if (gameControllerObject != null) {
+			gameController = gameControllerObject.GetComponent<GameController>();
+		}
+		if (gameController == null) {
+			Debug.Log ("Cannot find 'GameController' script");
+		}
+	}
+
 	void OnTriggerEnter(Collider other)
 	{
 		Debug.Log (other.name);
@@ -16,6 +30,7 @@ public class DestroyByContact : MonoBehaviour {
 		if (other.tag == "Player") {
 			Instantiate (playerExplosion, other.transform.position, other.transform.rotation);
 		}
+		gameController.AddScore (scoreValue);
 		Destroy (other.gameObject);
 		Destroy (gameObject);
 	}
